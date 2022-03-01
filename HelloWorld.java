ecd90333d72c0334ca79704fadaaf9015da489e1
unknown <maildikshasingh2207@gmail.com>
2022-03-01 13:05:14 +0530

    2nd commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,11 +1,10 @@
-
 public class HelloWorld{
 
+public static void main(String [] args){ 
 
 
- public static void main(String[] args){
-	System.out.println("Hello Diksha, welcome to java world");
-
- }
-
+  System.out.println("hello world from diksha");
+int i=10;
+int b=15;
+}
 }
